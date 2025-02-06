718de5d2323d77893ea33a547c20b2937e0c5ee2
LeoJu06 <leo.junghans6@gmail.com>
2025-02-06 16:36:49 +0100

    latest version after onedrive

--- src/mcts/node.py
+++ src/mcts/node.py
@@ -119,7 +119,9 @@ class Node:
 
         for move, prob in enumerate(action_probs):
             if prob > 0:
+                
                 self.children[move] = Node(prior=prob, to_play=-to_play)
+                
 
     def __repr__(self):
         """
--- src/mcts/worker.py
+++ src/mcts/worker.py
@@ -2,14 +2,11 @@ import time
 import numpy as np
 from src.mcts.mcts import MCTS
 from src.mcts.node import Node
-import torch
 import multiprocessing as mp
 from src.othello.othello_game import OthelloGame
 from src.config.hyperparameters import Hyperparameters
 from src.utils.dirichlet_noise import dirichlet_noise
 from src.utils.index_to_coordinates import index_to_coordinates
-import queue
-
 
 class Worker(MCTS):
     def __init__(self, worker_id, request_queue, response_queue, shared_array):
@@ -21,7 +18,7 @@ class Worker(MCTS):
 
     def request_manager(self, state):
         # Schreibe den Zustand des Spiels in den Shared Memory
-        self.shared_array[self.worker_id] = torch.tensor(state, dtype=torch.float32)
+        self.shared_array[self.worker_id] = state
 
         # Sende den Index des Zustands an den Manager
         self.request_queue.put((self.worker_id, self.worker_id))
@@ -29,12 +26,16 @@ class Worker(MCTS):
         # Warte auf die Antwort vom Manager
         while True:
             try:
-                response = self.response_queue.get(timeout=0.001)
+                response = self.response_queue.get(timeout=0.01)
                 if response["worker_id"] == self.worker_id:
                     return response["policy"], response["value"]
-            except queue.Empty:
+            except mp.queues.Empty:
                 continue
 
+
+
+
+    
     def expand_root(self, state: np.ndarray, to_play: int, add_dirichlet_noise: bool):
         """
         Expands the root node with initial action probabilities.
@@ -48,9 +49,7 @@ class Worker(MCTS):
         self.root.to_play = to_play
 
         # Convert the board to canonical form relative to the current player.
-        canonical_state = self.game.get_canonical_board(
-            self.root.state, self.root.to_play
-        )
+        canonical_state = self.game.get_canonical_board(self.root.state, self.root.to_play)
 
         action_probs, value = self.request_manager(canonical_state)
 
@@ -58,16 +57,10 @@ class Worker(MCTS):
             # Add Dirichlet noise to encourage exploration.
             action_probs = dirichlet_noise(action_probs)
 
-        valid_moves = self.get_valid_moves(
-            state, to_play
-        )  # Get valid moves for the current state.
-        action_probs = self.normalize_probs(
-            action_probs, valid_moves
-        )  # Normalize probabilities based on valid moves.
-        self.root.expand(
-            state, to_play, action_probs
-        )  # Expand the root node with action probabilities.
-
+        valid_moves = self.get_valid_moves(state, to_play)  # Get valid moves for the current state.
+        action_probs = self.normalize_probs(action_probs, valid_moves)  # Normalize probabilities based on valid moves.
+        self.root.expand(state, to_play, action_probs)  # Expand the root node with action probabilities.
+    
     def expand_leaf(self, leaf: Node, parent: Node, action: int) -> float:
         """
         Expands a leaf node by evaluating it or determining its value.
@@ -91,9 +84,7 @@ class Worker(MCTS):
 
         if value is None:
             # Predict value and action probabilities for the next state.
-            next_state_canonical = self.game.get_canonical_board(
-                next_state, player=leaf_player
-            )
+            next_state_canonical = self.game.get_canonical_board(next_state, player=leaf_player)
             action_probs, value = self.request_manager(next_state_canonical)
 
             # Filter probabilities by valid moves and expand the leaf node.
@@ -102,3 +93,4 @@ class Worker(MCTS):
             leaf.expand(next_state, leaf_player, action_probs)
 
         return value
+
