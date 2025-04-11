88e2ecf2239f262c956a5173fec2c7242db724ed
LeoJu06 <leo.junghans6@gmail.com>
2025-04-11 06:13:27 +0200

    new hyperparameter

--- src/config/hyperparameters.py
+++ src/config/hyperparameters.py
@@ -15,7 +15,7 @@ class Hyperparameters:
         - Node with keys ["key_passsing, prior_passing]"""
 
     MCTS = {
-        "num_simulations": 200,
+        "num_simulations": 50,
         "exploration_weight": 1.5,
         "temp_threshold": 14,
         "temp": 1,
@@ -23,20 +23,24 @@ class Hyperparameters:
     }
 
     Coach = {"iterations": 50, 
-             "episodes": 22*100,
+             "episodes": 22*50,
              "num_workers" :22, }
     Coach["episodes_per_worker"] = Coach["episodes"] // Coach["num_workers"]
 
     Neural_Network = {
         "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"), 
-        "epochs" : 20, 
-        "batch_size" : 128,
+        "epochs" : 600, 
+        "batch_size" : 512,
         "learning_rate" : 0.001
     }
 
     Node = {"key_passing": -1, "prior_passing": 1}
 
-    Arena = {"treshold": 0.55, "arena_games": 10}
+    Arena = {"treshold": 0.55, "arena_games": 150}
+
+
+def temperature(move_num, total_moves=60):
+    return max(0.1, 1.0 - move_num*0.03)  # Linear von 1.0 → 0.1 über 30 Züge
 
         
     
